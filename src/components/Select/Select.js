08da718b6506dc92687b3a4e0a1cfefab3e43f33
Annie Garvey <annierulz@msn.com>
2025-09-25 17:49:31 +0100

    feat: add Select (minus width fitting :( )

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -5,14 +5,50 @@ import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import { getDisplayedValue } from './Select.helpers';
 
-const Select = ({ label, value, onChange, children }) => {
+const Select = ({ label, value, onChange, children, className, ...rest }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <Wrapper className={className}>
+      <InnerSelect value={value} onChange={onChange} {...rest}>
+        {children}
+      </InnerSelect>
+      <Chevron id="chevron-down" strokeWidth={2}/>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+  position: relative;
+  width: fit-content;
+  color: ${COLORS.gray700};
+
+  &:hover {
+    color: black;
+  }
+`;
+
+const InnerSelect = styled.select`
+  font-family: Roboto, sans-serif;
+  font-size: 1rem;
+  padding: 12px 54px 12px 16px;
+  border: none;
+  border-radius: 8px;
+  background-color: ${COLORS.transparentGray15};
+  color: inherit;
+  appearance: none;
+  width: fit-content;
+
+  &:focus {
+    outline-color: ${COLORS.primary};
+  }
+`;
+
+const Chevron = styled(Icon)`
+  position: absolute;
+  top: 10px;
+  right: 12px;
+  pointer-events: none;
+`;
 
 export default Select;
