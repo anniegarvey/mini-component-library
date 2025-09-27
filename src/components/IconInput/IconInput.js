629d7eb0874be97d0506582b3fc061e13149a1a5
Annie Garvey <annierulz@msn.com>
2025-09-27 15:33:17 +0100

    feat: add IconInput

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -13,7 +13,51 @@ const IconInput = ({
   size,
   placeholder,
 }) => {
-  return 'TODO';
+  const isSmall = size === 'small';
+  return <Wrapper>
+    <label>
+      <VisuallyHidden>{label}</VisuallyHidden>
+      <StyledIcon id={icon} size={isSmall ? 16 : 24} strokeWidth={2} isSmall={isSmall} />
+      <Input placeholder={placeholder} isSmall={isSmall} width={width} />
+    </label>
+  </Wrapper>;
 };
+
+const Wrapper = styled.div`
+  position: relative;
+`;
+
+const StyledIcon = styled(Icon)`
+  position: absolute;
+  bottom: ${p => p.isSmall ? (4 / 16) : (5/16)}rem;
+  pointer-events: none;
+  color: ${COLORS.gray700};
+`;
+
+const Input = styled.input`
+  border: none;
+  border-block-end: ${p => p.isSmall ? 1 : 2}px solid ${COLORS.black};
+  color: ${COLORS.gray700};
+  font-family: Roboto, sans-serif;
+  font-size: ${p => p.isSmall ? (14 / 16) : (18 / 16)}rem;
+  font-weight: 700;
+  padding-inline-start: ${p => p.isSmall ? (24 / 16) : (36 / 16)}rem;
+  padding-block: 3px;
+  width: ${p => p.width}px;
+
+  &::placeholder {
+    color: ${COLORS.gray500};
+    font-family: Roboto, sans-serif;
+    font-weight: 400;
+  }
+
+  &:focus {
+    outline-offset: 2px;
+  }
+
+  &:hover {
+    color: ${COLORS.black};
+  }
+`;
 
 export default IconInput;
